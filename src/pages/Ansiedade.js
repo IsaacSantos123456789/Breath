327a732c3a0f9530e1604d93a089207812ae25d3
Isaac Mendes dos Santos <raaquelmeendes02@gmail.com>
2022-11-04 00:29:36 +0000

    Update 5 files

---
+++ src/pages/Ansiedade.js
@@ -0,0 +1,12 @@
+import "./Ansiedade.css";
+
+export default function Ansiedade() {
+  return (
+    <div>
+      <h1>Ansiedade</h1>
+      <p>
+        A ansiedade é um problema a ser resolvido
+      </p>
+    </div>
+  );
+}
